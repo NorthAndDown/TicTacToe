80fced4cd552d19f9f657ba6f6b6ab5f5020f9c4
Ingolfur <ingolfur@sigfusson.is>
2014-10-29 14:01:09 +0000

    First unit test added

--- src/main/java/com/northanddown/tictactoe/TicTacToe.java
+++ src/main/java/com/northanddown/tictactoe/TicTacToe.java
@@ -1,7 +1,11 @@
 package com.northanddown.tictactoe;
 
 public class TicTacToe {
-    public static void main(String[] args) {
-	    System.out.println("Tic Tac Toe");
-    }
+	public static void main(String[] args) {
+		System.out.println("Tic Tac Toe");
+	}
+	public static String test() {
+		return "Tic Tac Toe";
+		
+	}
 }
---
+++ src/test/java/com/northanddown/tictactoe/TicTacToeTest.java
@@ -0,0 +1,14 @@
+package com.northanddown.tictactoe;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TicTacToeTest {
+
+    @Test
+    public void firstTest() {
+        assertEquals("Tic Tac Toe", TicTacToe.test());
+    }
+
+}
