ba7894202dfc6a749d2b83cec949fa735efb91cd
Haraldur Skjóldal Kristjánsson <you@example.com>
2014-10-29 16:17:15 +0000

    added empty test class for Table

--- src/main/java/com/northanddown/tictactoe/Table.java
+++ src/main/java/com/northanddown/tictactoe/Table.java
@@ -2,23 +2,22 @@ package com.northanddown.tictactoe;
 
 public class Table {
 
-	private char[3][3]board;
+	private char[][] board = new char[3][3];
 	/*
 	TODO: núllstilla board array
 	*/   
 	public void initialize() {
 		for(int i = 0 ; i < 3 ; i++){
 			for(int j = 0 ; j < 3 ; j++){
-				board[i][j] = '';
+				// board[i][j] = '';
 			}
 		}
 	}
 
-	public void print(){
+/*	public void print(){
 	
 	}
 
-<<<<<<< HEAD
 	public void inputChecker(){
 
 	}
@@ -46,13 +45,6 @@ public class Table {
         private void insert(){
 
 	}
-=======
-	public void inputChecker(){}
-	public void isFull(){}
-	public void checkForWin(){}
-	private void checkHorWin(){}
-        private void checkVerWin(){}
-        private void checkDiaWin(){}
-        private void  insert(){}
->>>>>>> 882210dbc2dd072727a9f7c8d2aaf080db57436c
+
+	*/
 }
---
+++ src/test/java/com/northanddown/tictactoe/TestTable.java
@@ -0,0 +1,10 @@
+package com.northanddown.tictactoe;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestTable{
+//	@Test
+
+}
