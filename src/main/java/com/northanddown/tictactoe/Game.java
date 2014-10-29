d421d21769b9e877c9b6074cbfd03371df9ccb57
Ingolfur <ingolfur@sigfusson.is>
2014-10-29 17:54:17 +0000

    Game run() ready, added game loop in play()

--- src/main/java/com/northanddown/tictactoe/Game.java
+++ src/main/java/com/northanddown/tictactoe/Game.java
@@ -8,24 +8,35 @@ public class Game {
 	
 	//kallar i initialize og luppar(while table.notFull||!win)
 	public void run() {
-		welcomeMsg();
 		initTable();
 		initPlayers();
+		welcomeMsg();
+		play();
 		gameOverMsg();
-	}
-	private void welcomeMsg() {
-		System.out.println("======= Tic Tac Toe =======");
 	}
 	private void initTable() {
 		table = new Table();
-		table.print();
 	}
 	private void initPlayers() {
 		playerX = new Player('X');
 		playerO = new Player('O');
 		currentPlayer = playerX;
 	}
+	private void welcomeMsg() {
+		System.out.println("======= Tic Tac Toe =======");
+	}
 	private void gameOverMsg() {
 		System.out.println("=======  Game over  =======");
+	}
+	private void play() {
+		boolean playing = true;
+		while (playing) {
+			table.print();
+			printWhosTurn();
+			playing = false;
+		}
+	}
+	private void printWhosTurn() {
+		System.out.println("It is " + currentPlayer.getSign() + " turn to play");
 	}
 }
