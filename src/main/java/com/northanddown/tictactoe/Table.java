bedd99aa96037538ce7297e8d367386b4f492a10
Haraldur Skjóldal Kristjánsson <you@example.com>
2014-10-29 15:41:10 +0000

    added empty table class file

---
+++ src/main/java/com/northanddown/tictactoe/Table.java
@@ -0,0 +1,20 @@
+package com.northanddown.tictactoe;
+
+public class Table {
+
+	private char[3][3]board;
+	/*
+	TODO: núllstilla board array
+	*/   
+	public void initialize() {
+
+	}
+	public void print(){}
+	public void inputChecker(){}
+	public void isFull(){}
+	public void checkForWin(){}
+	private void checkHorWin(){}
+        private void checkVerWin(){}
+        private void checkDiaWin(){}
+        private void insert(){}
+}
