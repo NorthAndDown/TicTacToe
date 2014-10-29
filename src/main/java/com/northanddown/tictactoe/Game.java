22486e09f7b9aa58cabae1ffeeeebdab22cd5bed
Ingolfur <ingolfur@sigfusson.is>
2014-10-29 17:15:06 +0000

    Game class: declare class vars, call table.print()

--- src/main/java/com/northanddown/tictactoe/Game.java
+++ src/main/java/com/northanddown/tictactoe/Game.java
@@ -1,9 +1,16 @@
 package com.northanddown.tictactoe;
 
 public class Game {
+	private Table table;
+	private Player playerX;
+	private Player playerO;
+	private Player currentPlayer;
 	
 	//kallar i initialize og luppar(while table.notFull||!win)
 	public void start() {
-		System.out.println("Start the Tic Tac Toe game");
+		System.out.println("======= Tic Tac Toe =======");
+		table = new Table();
+		table.print();
+		
 	}
 }
