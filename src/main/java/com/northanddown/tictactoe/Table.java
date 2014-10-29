caf0a7049040287dfad03831e5445fff3e39b80c
Haraldur Skjóldal Kristjánsson <you@example.com>
2014-10-29 18:34:42 +0000

    Added a Table Constuctor

--- src/main/java/com/northanddown/tictactoe/Table.java
+++ src/main/java/com/northanddown/tictactoe/Table.java
@@ -1,19 +1,22 @@
 package com.northanddown.tictactoe;
 
 public class Table {
+	private char[][] board;
 
-	private char[][] board = new char[3][3];
-	  
+	public Table(){
+		board = new char[3][3];
+		initialize();
+	}
+
 	public void initialize() {
 		for(int i = 0 ; i < 3 ; i++){
 			for(int j = 0 ; j < 3 ; j++){
-				 board[i][j] = '.';
+				 board[i][j] = ' ';
 			}
 		}
 	}
 
 	public void print(){
-		initialize();
 		System.out.println("   0   1   2");
 		System.out.println(" -------------");
                 for(int i = 0 ; i < 3 ; i++){
@@ -27,20 +30,24 @@ public class Table {
 	}
 
 	public boolean inputChecker(int x, int y){
-		initialize();
 		if(x < 0 || x > 2 || y < 0 || y > 2){
-			System.out.println("Fyrri if setningin");
+			//System.out.println("Fyrri if setningin");
 			return false;
 		}
-		if(board[y][x] == '.'){
-			System.out.println("Seinni if setnign");
+		if(board[y][x] == ' '){
+			//System.out.println("Seinni if setnign");
 			return true;
 		}
-		System.out.println("Seinasta skilyrdid");
+		//System.out.println("Seinasta skilyrdid");
 		return false;
 	}
 
 	public void isFull(){
+		for(int i = 0 ; i < 3 ; i++){
+                        for(int j = 0 ; j < 3 ; j++){
+                                 //if(board[i][j] = ' ');
+                        }
+                }
 
 	}
 
