19a290d4f34ddef46686e28b56005e0a963ec7dd
Ingolfur <ingolfur@sigfusson.is>
2014-11-04 17:14:40 +0000

    Adding comment to the game class

--- src/main/java/com/northanddown/tictactoe/TicTacToe.java
+++ src/main/java/com/northanddown/tictactoe/TicTacToe.java
@@ -2,6 +2,7 @@ package com.northanddown.tictactoe;
 
 public class TicTacToe {
     public static void main(String[] args) {
+	//Create instance of the game and then run it
         Game game = new Game();
         game.run();
     }
